df2f2f7f96234b414ab5bfbf5cf3094c0ee800b1
harvest3155 <harvest3155@yahoo.com>
2018-08-21 11:22:11 -0400

    Update entry_form.py

--- entry_form.py
+++ entry_form.py
@@ -1,5 +1,7 @@
 import tkinter
 from tkinter import *
+#import psycopg2
+
 
 window= Tk()
 
@@ -15,15 +17,22 @@ def clear_all():
     Name_text.set("Name")
 
 def enter_all():
-    bills_ent= e1.get()
-    comp_ent= e2.get()
+    bills_ent= e1.get().title()
+    comp_ent= e2.get().title()
     amount_ent = e3.get()
     due_ent= e4.get()
     end_ent=e5.get()
     auto_chk= Auto_Pay.get()
     ongoing_chk =Ongoing_pay.get()
     name_sel=Name_text.get()
-    
+    pay_sel=Payment_type.get()
+
+    if amount_ent.isnumeric() != True:
+        print("Amount is not a Number")
+        sys.exit()
+        
+##    last4=
+##    
     print(bills_ent)
     print(comp_ent)
     print(amount_ent)
@@ -33,12 +42,27 @@ def enter_all():
     print(ongoing_chk)
     print(name_sel)
 
+##    conn = psycopg2.connect("dbname='Andy' user='postgres' host='localhost'\
+##    password='S3155'")
+##    cur = conn.cursor()
+##
+##    cur.execute('INSERT INTO bill.monthly_bills(company_name,bill_name,\
+##    amount_due_monthly,account, user_name,due_date,end_date,reocurring,autopay) VALUES (%s, %s, %s, %s, %s, %s,\
+##    %s, %s, %s)', (comp_ent, bills_ent, amount_ent ,'c',name_sel,due_ent,end_ent,  ongoing_chk, auto_chk))
+
+##    conn.commit()
+##    cur.close()
+##    conn.close()
+##    print("Data submitted")
+
+
+
+
+
 def close_window():
     window.destroy()
     
 #labels
-L1=Label(window, text="Name")
-L1.grid(row=1,column=2)
 
 L1=Label(window, text="Bills")
 L1.grid(row=1,column=0)
@@ -82,22 +106,31 @@ e5.grid(row=5,column=1)
 Auto_Pay=IntVar()
 e6=Checkbutton(window,text="Auto-Pay",variable=Auto_Pay,\
                onvalue=1,offvalue=0,\
-               height = 1, width =10)
+               height = 0, width =10)
 e6.grid(row=4,column=2,sticky = 'w')
 
 Ongoing_pay=IntVar()
 e7=Checkbutton(window,text="On-going Payment",variable=Ongoing_pay,\
                onvalue=1,offvalue=0,\
-               height = 1, width =18)
+               height = 0, width =18)
 e7.grid(row=5,column=2,sticky = 'w')
 
 #dropdown
 Name={"Andy","Jackie"}
+CC={"5/3 Checking","5/3 Credit", "Chase Checking","Chase Credit"}
+sorted(Name)
+sorted(CC)
 
 Name_text=StringVar()
 e8=OptionMenu(window, Name_text,*Name)
 e8.grid(row=1,column=3)
 Name_text.set("Name")
+
+Payment_type=StringVar()
+e9=OptionMenu(window, Payment_type,*CC)
+e9.grid(row=1,column=2)
+Payment_type.set("Payment Type")
+    
 #buttons
 b1 =Button(window,text="Clear", width=10,command=clear_all)
 b1.grid(row=6,column=3)
@@ -111,3 +144,4 @@ b3.grid(row=6,column=5)
 
 
 window.mainloop()
+
